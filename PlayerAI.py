2b8acc163ffde0d57a10de013da44ee18cb3340d
JUDY HANWNEN SHEN <judyshen.hw@gmail.com>
2014-09-20 23:26:48 -0400

    new inclination

--- PlayerAI.py
+++ PlayerAI.py
@@ -14,7 +14,7 @@ class PlayerAI():
         my_x = my_position[0]
         my_y = my_position[1]
         my_direction = player_lightcycle['direction']
-        randMove = self.next_move(my_x, my_y, game_map, my_direction)
+        randMove = self.next_move(my_x, my_y, game_map, my_direction, player_lightcycle)
         if randMove == 3:
             return PlayerActions.MOVE_LEFT
         elif randMove == 1:
@@ -23,42 +23,40 @@ class PlayerAI():
             return PlayerActions.MOVE_DOWN
         elif randMove == 0:
             return PlayerActions.MOVE_UP
+        elif randMove == 5:
+            return PlayerActions.ACTIVATE_POWERUP
         else:
-            if player_lightcycle["hasPowerup"]:
-                return PlayerActions.ACTIVATE_POWERUP
-            else:
-                print "Nice Try!"
-                print randMove
-                return PlayerActions.MOVE_UP
+            print "Nice Try!"
+            return PlayerActions.MOVE_UP
 
-    def next_move(self, x, y, game_map, direction):
+    def next_move(self, x, y, game_map, direction, player_lightcycle):
         #checks current direction
         if direction == 2:
             down = self.check_down(x, y, game_map, "all")
             if down and self.not_dead_end(x, y, game_map, 2) and self.peek(x,y,game_map, 2):
                 return 2
             else:
-                return self.find_next_step(x, y, game_map, 2)
+                return self.find_next_step(x, y, game_map, 2, player_lightcycle)
         elif direction == 3:
             left = self.check_left(x, y, game_map, "all")
             if left and self.not_dead_end(x, y, game_map, 3) and self.peek(x,y,game_map, 3):
                 return 3
             else:
-                return self.find_next_step(x, y, game_map, 3)
+                return self.find_next_step(x, y, game_map, 3, player_lightcycle)
         elif direction == 1:
             right = self.check_right(x, y, game_map, "all")
             if right and self.not_dead_end(x, y, game_map, 1) and self.peek(x,y,game_map, 1):
                 return 1
             else:
-                return self.find_next_step(x, y, game_map, 1)
+                return self.find_next_step(x, y, game_map, 1, player_lightcycle)
         else:
             up = self.check_up(x, y, game_map, "all")
             if up and self.not_dead_end(x, y, game_map, 0) and self.peek(x,y,game_map, 0):
                 return 0
             else:
-                return self.find_next_step(x, y, game_map, 0)
+                return self.find_next_step(x, y, game_map, 0, player_lightcycle)
 
-    def find_next_step(self, x, y, game_board, direction):
+    def find_next_step(self, x, y, game_board, direction, player_lightcycle):
         down = self.check_down(x,y, game_board, "all")
         up = self.check_up(x, y, game_board, "all")
         right = self.check_right(x, y, game_board, "all")
@@ -69,7 +67,7 @@ class PlayerAI():
             elif not up and down:
                     return 2
             elif not up and not down:
-                    return 5
+                    return self.check_tricks(x, y, game_board, player_lightcycle)
             else:
                 return self.vision(x,y,game_board,direction)
         else:
@@ -78,9 +76,26 @@ class PlayerAI():
             elif not left and right:
                     return 1
             elif not left and not right:
-                    return 5
+                    return self.check_tricks(x, y, game_board, player_lightcycle)
             else:
                 return self.vision(x,y,game_board,direction)
+
+    def check_tricks(self, x, y, game_board, player_lightcycle):
+        has_power = player_lightcycle["hasPowerup"]
+        is_invincible = player_lightcycle["isInvincible"]
+        if is_invincible:
+            if not self.check_left(x, y, game_board, "o_trail"):
+                return 3
+            if not self.check_up(x, y, game_board, "o_trail"):
+                return 0
+            if not self.check_right(x, y, game_board, "o_trail"):
+                return 1
+            if not self.check_down(x, y, game_board, "o_trail"):
+                return 2
+        elif has_power:
+            return 5
+        else:
+            return 6
 
     def vision(self, x, y, game_board, direction):
         count1 = 0
@@ -143,6 +158,13 @@ class PlayerAI():
                 return False
             else:
                 return True
+
+        elif check_type == "o_trail":
+            print game_board[x-1][y]
+            if game_board[x-1][y] == TRAIL:
+                return False
+            else:
+                return True
         else:
             if game_board[x-1][y] != EMPTY and game_board[x-1][y] != POWERUP:
                 return False
@@ -155,6 +177,13 @@ class PlayerAI():
 
         elif check_type == "opponent":
             if game_board[x+1][y] == LIGHTCYCLE:
+                return False
+            else:
+                return True
+
+        elif check_type == "o_trail":
+            print game_board[x+1][y]
+            if game_board[x+1][y] == TRAIL:
                 return False
             else:
                 return True
@@ -173,6 +202,12 @@ class PlayerAI():
                 return False
             else:
                 return True
+
+        elif check_type == "o_trail":
+            if game_board[x][y+1] == TRAIL:
+                return False
+            else:
+                return True
         else:
             if game_board[x][y+1] != EMPTY and game_board[x][y+1] != POWERUP:
                 return False
@@ -186,6 +221,12 @@ class PlayerAI():
 
         elif check_type == "opponent":
             if game_board[x][y-1] == LIGHTCYCLE:
+                return False
+            else:
+                return True
+
+        elif check_type == "o_trail":
+            if game_board[x][y-1] == TRAIL:
                 return False
             else:
                 return True
